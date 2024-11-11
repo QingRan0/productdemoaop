73a43dc3f265f44e65e62e0a3fa6b67490118b19
Qing Ran <165892146+xmusys@users.noreply.github.com>
2024-11-11 14:41:11 +0800

    修正

--- mysql/mysql-docker.sh
+++ mysql/mysql-docker.sh
@@ -2,4 +2,4 @@
 docker pull mysql
 docker service create --name mysql --constraint node.labels.database==mysql --mount type=bind,source=/root/JavaEEPlatform/mysql/sql,destination=/sql,readonly --mount type=bind,source=/root/JavaEEPlatform/mysql/conf.d,destination=/etc/mysql/conf.d,readonly --network my-net -e MYSQL_ROOT_PASSWORD=123456 -d mysql:latest
 docker service ps mysql
-docker exec -it mysql -uroot -p
+docker exec -it mysql -udemouser -p
